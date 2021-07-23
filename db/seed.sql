1a3795aa4da9ef94f5f3f1aba5dded2e194e1c36
Melissa Brewer <melramirez727@gmail.com>
2021-07-22 21:29:03 -0500

    db tables

---
+++ db/seed.sql
@@ -0,0 +1,4 @@
+INSERT INTO role(title, salary, department_id)
+VALUES("lead engineer", 150000, 2), ("sales leads", 100000, 1),("lawyer", 190000, 4), ("accountant", 125000, 3), ("legal team lead", 250000, 4), ("software emgineer", 120000, 2), ("salesperson", 80000, 1);
+
+SELECT * FROM role
