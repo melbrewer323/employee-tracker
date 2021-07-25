ee21f90cd30bfbf88c98d9f24f79b0bef634b221
Melissa Brewer <melramirez727@gmail.com>
2021-07-25 01:11:43 -0500

    added answer.action

--- server.js
+++ server.js
@@ -4,11 +4,16 @@ const consoleTable = require('console.table');
 
 const connection = mysql.createConnection({
     host: 'localhost',
-    port: 3001,
+    port: 3306,
     user: 'root',
     password: 'Root',
     database: 'empTracDB',
 });
+
+connection.connect((err) => {
+    if (err) throw err;
+    start();
+  });
 
 const start = () => {
     inquirer
@@ -17,8 +22,36 @@ const start = () => {
             type: 'list',
             name: 'toDo',
             message: "What would you like to do?",
-            choices: ["Add department", "Add role", "Add Employee", "View departments", "View roles", "View employees", "Update employee roles", "Exit"]
+            choices: ["Add department", "Add role", "Add Employee", "View departments", "View roles", "View employees", "Update employee role", "Exit"]
         },
      
-    ]);
+    ])
+    .then((anser) => {
+        switch (anser.action) {
+            case 'Add department':
+                addDepartment();
+                break;
+            case 'Add role':
+                addRole();
+                break;
+            case 'Add employee':
+                addEmployee();
+                break;
+            case 'View departments':
+                viewDepartments();
+                break;
+            case 'View roles':
+                viewRoles();
+                break;
+            case 'View employees':
+                viewEmployees();
+                break;
+            case 'Update employee role':
+                updateEmployeeRole();
+                break;
+            case 'Exit':
+                Exit();
+                break;                            
+        }
+    });
 };
